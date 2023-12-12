23158e99c99abc0880d68c597c509c01297a4619
Taha Ahmet Ok <taha.ahmetok@gmail.com>
2023-12-12 15:35:30 +0300

    Delete operation was edited

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,43 +3,96 @@ package org.example
 import org.example.Models.Student
 
 fun main() {
-    var idCounter = 0
-    val studentList = mutableListOf<Student>()
+    val students = mutableListOf<Student>()
+
     while (true) {
-        println("Name: ")
-        val nameInput = readLine() ?: ""
+        println("----- MENU -----")
+        println("1. Add Student")
+        println("2. Sort by TYT Score")
+        println("3. Sort by AYT Score")
+        println("4. Delete Student")
+        println("0. Exit")
 
-        println("Surname: ")
-        val surnameInput = readLine() ?: ""
-
-        println("School Number: ")
-        val schoolNumberInput = readLine() ?: ""
-
-        println("TYT Grade: ")
-        val tytGradeInput = readLine() ?: ""
-
-        println("AYT Grade: ")
-        val aytGradeInput = readLine() ?: ""
-
-        println("Target Job: ")
-        val targetJobInput = readLine() ?: ""
-
-        val newStudent = Student(
-            idCounter,
-            nameInput,
-            surnameInput,
-            schoolNumberInput,
-            tytGradeInput.toDouble(),
-            aytGradeInput.toDouble(),
-            targetJobInput
-        )
-        studentList.addFirst(newStudent)
-        //veri tabanına ekle
-        idCounter++
-        studentList.forEach {
-            println(it.toString())
+        print("Option: ")
+        when (readLine()?.toIntOrNull()) {
+            1 -> addStudent(students)
+            2 -> sortByTytGrade(students)
+            3 -> sortByAytGrade(students)
+            4 -> removeStudent(students)
+            0 -> return
+            else -> println("Invalid option. Please try again.")
         }
+
+        println()
+    }
+}
+
+
+fun addStudent(students: MutableList<Student>) {
+
+    print("Student Name: ")
+    val name = readLine() ?: ""
+    print("Student Surname: ")
+    val surname = readLine() ?: ""
+    print("School Number: ")
+    val schoolNumber = readLine() ?: ""
+    print("TYT Grade: ")
+    val tytGrade = readLine()?.toDoubleOrNull() ?: 0.0
+    print("AYT Grade: ")
+    val aytGrade = readLine()?.toDoubleOrNull() ?: 0.0
+    print("Target Job: ")
+    val targetJob = readLine() ?: "undefined"
+
+
+    val newStudent = Student(
+        id = students.size + 1,
+        name = name,
+        surname = surname,
+        schoolNumber = schoolNumber,
+        tytGrade = tytGrade.toString().toDouble(),
+        aytGrade = aytGrade.toString().toDouble(),
+        targetJob = targetJob
+    )
+
+    students.add(newStudent)
+    println("The student added successfully.")
+
+
+}
+
+fun sortByTytGrade(students: List<Student>) {
+    val sortedStudents = students.sortedByDescending { it.tytGrade }
+    printStudents("Ranking for TYT Grades", sortedStudents)
+}
+
+fun sortByAytGrade(students: List<Student>) {
+    val sortedStudents = students.sortedByDescending { it.aytGrade }
+    printStudents("Ranking for AYT Grades", sortedStudents)
+}
+
+fun printStudents(title: String, students: List<Student>) {
+    println("----- $title -----")
+    students.forEachIndexed { index, student ->
+        println(
+            "${index + 1}. ${student.name} ${student.surname} - School Number: ${student.schoolNumber},  TYT Grade: ${student.tytGrade}, AYT Grade: ${student.aytGrade}, Target Job: ${student.targetJob}"
+        )
     }
 
+}
 
+fun removeStudent(students: MutableList<Student>) {
+    print("Enter the school number of the student you want to remove: ")
+    val schoolNumber = readLine()?.toIntOrNull()
+
+    if (schoolNumber != null) {
+        val removedStudent = students.find { it.schoolNumber == schoolNumber.toString() }
+        if (removedStudent != null) {
+            students.remove(removedStudent)
+            println("Student successfully removed.")
+        } else {
+            println("No student found with the specified school number.")
+        }
+    } else {
+        println("Invalid school number input.")
+    }
 }
